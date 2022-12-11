ba6f2974e2c403310c524cf4a059c0b1cad96dd4
pradeepdsmk <pradeepdsmk@gmail.com>
2022-12-11 13:55:01 +0530

    added job and education locations with images

--- js/script.js
+++ js/script.js
@@ -1,54 +1,58 @@
-$(function () {
-    // PRELOADER
-    $(window).on('load', function () {
-        $('#page-loader').fadeOut('slow', function () {
-            $(this).remove();
-        });
-    });
+// window.addEventListener('DOMContentLoaded', function () {
+    $(function () {
+        // PRELOADER
+        // $(window).on('load', function () {
+            console.log('fading out spinner')
+            $('#page-loader').fadeOut('slow', function () {
+                $(this).remove();
+            });
+        // });
 
-    // navbarDropdown
-    if ($(window).width() < 992) {
-        $('.has-dropdown .dropdown-toggle').on('click', function () {
-            $(this).siblings('.dropdown-menu').animate({
-                height: 'toggle'
-            }, 300);
-        });
-    }
-
-
-    // SCROLL TO TOP
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop() > 70) {
-            $('.scroll-to-top').addClass('reveal');
-        } else {
-            $('.scroll-to-top').removeClass('reveal');
+        // navbarDropdown
+        if ($(window).width() < 992) {
+            $('.has-dropdown .dropdown-toggle').on('click', function () {
+                $(this).siblings('.dropdown-menu').animate({
+                    height: 'toggle'
+                }, 300);
+            });
         }
-    });
 
 
-    // Fixed header
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop() > 70) {
-            $('.site-navigation,.trans-navigation').addClass('header-white');
-        } else {
-            $('.site-navigation,.trans-navigation').removeClass('header-white');
-        }
-    });
-
-
-    // scroll-to-top
-    if ($('#scroll-to-top').length) {
-        $('#scroll-to-top').on('click', function () {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 600);
-            return false;
+        // SCROLL TO TOP
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 70) {
+                $('.scroll-to-top').addClass('reveal');
+            } else {
+                $('.scroll-to-top').removeClass('reveal');
+            }
         });
-    }
 
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').on('click', function (event) {
-        $('.navbar-collapse').collapse('hide');
-    });
-})
+        // Fixed header
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 70) {
+                $('.site-navigation,.trans-navigation').addClass('header-white');
+            } else {
+                $('.site-navigation,.trans-navigation').removeClass('header-white');
+            }
+        });
+
+
+        // scroll-to-top
+        if ($('#scroll-to-top').length) {
+            $('#scroll-to-top').on('click', function () {
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 600);
+                return false;
+            });
+        }
+
+
+        // Closes responsive menu when a scroll trigger link is clicked
+        $('.js-scroll-trigger').on('click', function (event) {
+            $('.navbar-collapse').collapse('hide');
+        });
+    })
+// })
+console.log('script loaded')
